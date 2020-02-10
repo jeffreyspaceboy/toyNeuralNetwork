756c61f0acc75a76666a8c531cb3eb94fc0811fc
Jeffrey Fisher II <jeffreyspaceboy@gmail.com>
2020-02-10 15:07:52 -0600

    Initial Commit

---
+++ toyNN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  toyNN
+//
+//  Created by Jeffrey Fisher on 2/10/20.
+//  Copyright © 2020 Jeffrey Fisher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
